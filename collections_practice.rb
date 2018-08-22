aca5d274c3df387d1705f32324354c08cd481208
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:54:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,7 +32,7 @@ end
 
 def remove_non_strings(array)
   array.each do |item|
-    if item.is_a?(String) 
+    if item.respond_to?(:to_s)
     elsif item.is_a?(Key)
     array.delete(item)
    else array.delete(item)
