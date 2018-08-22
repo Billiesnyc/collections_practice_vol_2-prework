ff95e9068a2c3a6b14c4420b10e5fef1e0bae4ab
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 14:58:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,7 +33,7 @@ end
 def remove_non_strings(array)
   array.each do |item|
     if item.class == Fixnum
-    elsif item.start_with? ":"
+    elsif item.class == Symbol
     array.delete(item)
     end
   end
